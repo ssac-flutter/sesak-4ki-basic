b7509773a41a2847bec059d27da286a0dafc51d7
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-07-29 16:18:56 +0900

    카운터앱에 Stream listen 방식

--- lib/counter_stream/counter_stream_app.dart
+++ lib/counter_stream/counter_stream_app.dart
@@ -1,10 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_pr_guide/counter_stream/counter_api.dart';
 
-class CounterStreamApp extends StatelessWidget {
-  CounterStreamApp({Key? key}) : super(key: key);
+class CounterStreamApp extends StatefulWidget {
+  const CounterStreamApp({Key? key}) : super(key: key);
 
+  @override
+  State<CounterStreamApp> createState() => _CounterStreamAppState();
+}
+
+class _CounterStreamAppState extends State<CounterStreamApp> {
   final _api = CounterApi();
+
+  int _count = 0;
+
+  StreamSubscription<int>? _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = _api.countStream.listen((count) {
+      setState(() {
+        _count = count;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // if (_subscription != null) {
+    //   _subscription!.cancel();
+    // }
+    _subscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +43,10 @@ class CounterStreamApp extends StatelessWidget {
         title: const Text('카운터 Stream 버전'),
       ),
       body: Center(
-        child: StreamBuilder<int>(
-            initialData: 0,
-            stream: _api.countStream,
-            builder: (context, snapshot) {
-              return Text(
-                '${snapshot.data}',
-                style: Theme.of(context).textTheme.headline1,
-              );
-            }),
+        child: Text(
+          '$_count',
+          style: Theme.of(context).textTheme.headline1,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
