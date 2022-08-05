2ed802cb6daf72775a0c260a8746cd7b4a69a895
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-08-05 17:13:08 +0900

    feat: 구글 로그인, 로그아웃

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_pr_guide/book_list_app/book_list/book_list_screen.dart';
+import 'package:flutter_pr_guide/book_list_app/root/root_screen.dart';
 import 'package:flutter_pr_guide/image_search_app/color_schemes.g.dart';
 
 import 'firebase_options.dart';
@@ -13,13 +13,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  test().then((value) => null).catchError((e) => print(e));
-
   runApp(const MyApp());
-}
-
-Future test() async {
-  throw 'test';
 }
 
 class MyApp extends StatelessWidget {
@@ -37,7 +31,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: darkColorScheme,
       ),
-      home: BookListScreen(),
+      home: const RootScreen(),
     );
   }
 }
